c58807663aa3c2b9d42a3e4052242a1230573f90
RobinDouma <robin.douma@hotmail.com>
2021-05-28 16:24:29 +0200

    Streamlit works!!

--- uvvis_plotter_multiple.py
+++ uvvis_plotter_multiple.py
@@ -1,8 +1,10 @@
 from matplotlib import pyplot as plt
+from matplotlib import cm
 import csv
 import numpy as np
 import glob
-
+import xlsxwriter
+import os
 
 class Spectrum:
     """Creates an instance of a made shape"""
@@ -17,14 +19,14 @@ class Spectrum:
 # Change the file name and directory here for calculation
 file_extension = 'csv'
 file_dir = 'C:\\Users\\s152191\\OneDrive - TU Eindhoven\\Master\\0. Afstuderen\\3 - Experimental data\\9. UV-Vis PE\\' \
-           '210421 CLCEA2.1\\210526 210520 CLCEA2.1 lines - Reflection at RT\\'
+           '210421 CLCEA2.1\\210526 210518&20 CLCEA2.1 lines - Reflection at RT\\40m\\'
 file_names = glob.glob(file_dir + '*.{}'.format(file_extension))
 cutoff = 600  # nm
 spectra = [0] * len(file_names)
-
+# Writes incomprehensible data into usable classes
 for file_i, file_name in enumerate(file_names):
     x, y = [], []
-    name = file_name[161:-15]
+    name = file_name[181:-15]
     # Extracts the values from the .csv file
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
@@ -44,17 +46,40 @@ for file_i, file_name in enumerate(file_names):
     y_max_index = y.index(max(y))
     x_max = x[y_max_index]
     y_max = y[y_max_index]
-    print("The spectrum of:\n%s%s.dat\n, has the following properties [μm]:\nMax reflectance: %f %% at %f nm"
-          % (file_dir, file_name, y_max, x_max))
     spectra[file_i] = Spectrum(name, x, y, y_max_index, x_max, y_max)
 
 # Plots the profile
-for spectrum in spectra:
-    plt.plot(spectrum.x, spectrum.y)
+viridis = cm.get_cmap('viridis', len(spectra))
+for spectrum_i, spectrum in enumerate(spectra):
+    color = spectrum_i / len(spectra)
+    plt.plot(spectrum.x, spectrum.y, label=spectrum.id, color=viridis(color))
     plt.xlabel("Wavelength [nm]")
     plt.ylabel("Reflectance [%]")
-    plt.title("Reflectance of %s" % spectrum.id)
-    plt.text(spectrum.x_max, spectrum.y_max, " at " + str(int(spectrum.x_max)) + " nm")
-    plt.text(spectrum.x_max + 20, spectrum.y_max, str(int(spectrum.y_max)) + " %")
-plt.show()
-# plt.savefig(file_dir + file_name + ".png")
+    plt.title("Reflectance")
+    plt.legend()
+if os.path.exists(file_dir + "plot" + ".png"):  # Deletes file if preexisting
+    os.remove(file_dir + "plot" + ".png")
+plt.savefig(file_dir + "plot" + ".png")
+
+# Writes an .xlsx file for important values and a proper table of all values
+if os.path.exists(file_dir + '\\extra\\output_python.xlsx'):  # Deletes file if preexisting
+    os.remove(file_dir + '\\extra\\output_python.xlsx')
+workbook = xlsxwriter.Workbook(file_dir + '\\extra\\output_python.xlsx')
+worksheet_max = workbook.add_worksheet('Max')
+worksheet_gen = workbook.add_worksheet('General')
+worksheet_max.write(0, 0, 'ID')
+worksheet_max.write(0, 1, 'Peak [nm]')
+worksheet_max.write(0, 2, 'Reflectance [%R]')
+worksheet_gen.write(0, 0, 'Wavelength [nm')
+for line, spec in enumerate(spectra):
+    worksheet_max.write(line + 1, 0, spec.id)
+    worksheet_max.write(line + 1, 1, spec.x_max)
+    worksheet_max.write(line + 1, 2, spec.y_max)
+    worksheet_gen.write(0, line + 1, spec.id)
+    for y_i, y_point in enumerate(spec.y):
+        if line != 0:
+            worksheet_gen.write(y_i + 1, line + 1, spec.y[y_i])
+        else:
+            worksheet_gen.write(y_i + 1, line, spec.x[y_i])
+            worksheet_gen.write(y_i + 1, line + 1, spec.y[y_i])
+workbook.close()
